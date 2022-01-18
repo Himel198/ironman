6c4a12befc3e7f90397adce8cd8fd8174bef937d
Himel <92205520+Himel198@users.noreply.github.com>
2022-01-18 15:44:39 +0600

    Update iman.py

--- iman.py
+++ iman.py
@@ -1,10 +1,10 @@
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
-import datetime
-import wikipedia #pip install wikipedia
-import webbrowser
-import os
-import smtplib
+#pip install pyttsx3
+#pip install speechRecognition
+
+
+
+
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
